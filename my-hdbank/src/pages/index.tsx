2c438678e3b19d5e4380abbb4043922ebb2e9a0d
Lam Nguyen <nguyentunglamvnm@gmail.com>
2025-09-22 14:29:56 +0700

    init

--- my-hdbank/src/pages/index.tsx
+++ my-hdbank/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { AppProvider } from "@/lib/redux/provider";
 import CreditCardIssueScreen from "./trang-chu/trang-chu-dop";
+import { DOPIntroScreen } from "@/pages/dop-intro";
 
 
 function HomePage() {
   return (
     <AppProvider>
-      <CreditCardIssueScreen />
+      <DOPIntroScreen />
     </AppProvider>
   );
 }
